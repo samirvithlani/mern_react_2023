704df9513ea11c5b82368d33e531ff8c50e60a38
samirvithlani <samir.vithlani83955@gmail.com>
2023-11-22 20:48:39 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var name ="Kohli"
+  var runs = 10000
+  var isMarried = true
+  var age = 30
+  //json...
+  var score={
+    test:21,
+    odi:50,
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>HELLO ALL....</h1>
+      {name}
+      <h1>Name = {name}</h1>
+      <h1>Runs = {runs}</h1>
+      <h1>isMarried = {isMarried == true ? "YES" : "NO"}</h1>
+      <h1>ODI score = {score.odi}</h1>
+      <h1>Test score = {score.test}</h1>
+      <h1>Age = {age}</h1>
     </div>
   );
 }
